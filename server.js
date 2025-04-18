8ba59a513cc52f998e5db94b29509266e6c87300
zoargap <zoargap@gmail.com>
2025-04-18 15:19:51 +0530

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Your tokens and their expiry dates
 const tokens = {
